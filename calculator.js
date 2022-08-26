9da00608666f5f5625b63773072277be1e1a4166
Paradox <tananhdt99@gmail.com>
2022-08-26 10:08:38 +0700

    add calculator.js

--- calculator.js
+++ calculator.js
@@ -1,3 +1,41 @@
 class Calculator {
-
+  calculate(number1, number2, method) {
+    method.cal();
+  }
 }
+
+class Addition {
+  cal(number1, number2){
+    return number1 + number2;
+  }
+}
+
+class Subtraction {
+  cal(number1, number2){
+    return number1 - number2;
+  }
+}
+
+class Division  {
+  cal(number1, number2){
+    if(number2 === 0) {
+      return "khong thuc hien duoc";
+    } 
+    else {
+      return number1 / number2;
+    }
+  }
+}
+
+class Multiple {
+  cal(number1, number2){
+    return number1 * number2;
+  }
+}
+
+
+let calculator = new Calculator();
+let addition = new Addition();
+
+let abc = calculator.calculate(12, 13, addition);
+console.log(abc);
